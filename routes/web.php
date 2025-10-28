a8865ab81af1240c4980f1d8a86db1a55b0196b2
Mario <alxmario.tejada@gmail.com>
2025-10-27 19:40:13 -0600

    [200~

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\AccesoController;
+use App\Http\Controllers\Auth\UsuarioController;
+use App\Http\Controllers\TipoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +21,15 @@ use App\Http\Controllers\Auth\AccesoController;
     return view('welcome');
 });*/
 
-Route::get('/',[AccesoController::class, 'mostrarFormulario'])->name('acceso');
-Route::post('/login',[AccesoController::class, 'iniciarSesion'])->name('login');
+Route::get('/', [AccesoController::class, 'mostrarFormulario'])->name('acceso');
+Route::post('/login', [AccesoController::class, 'iniciarSesion'])->name('login');
+Route::post('/logout', [AccesoController::class, 'cerrarSesion'])->name('logout');
+
+// Rutas protegidas
+Route::middleware('auth')->group(function(){
+    Route::resource('usuarios', UsuarioController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+});
+
+Route::middleware('auth')->group(function(){
+    Route::resource('tipos', TipoController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+});
